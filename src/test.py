eb696e5c9642a10f43d3bd74b725804ab058543b
Ismail Samil <ismsamil@emeal.nttdata.com>
2026-06-10 00:29:49 +0100

    refactoring  orchestrator

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ if __name__=="__main__":
     with open("pyproject.toml", "rb") as f:
      doc = tomlrt.load(f)
 
-    print(doc.get("paths")["main_file"])
+    print(doc.get("_MASK_TO_STRFTIME"))
     # doc["project"]["version"] = "0.2.0"
     # doc["project"]["dependencies"].append("requests>=2")
 
